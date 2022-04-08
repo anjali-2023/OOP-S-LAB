231a2b49ce2e866b1b1f8300ce92a291e10118f0
anjali-2023 <101088770+anjali-2023@users.noreply.github.com>
2022-04-08 09:21:24 +0530

    Add files via upload

---
+++ CO1/Cpu.java
@@ -0,0 +1,45 @@
+
+package cpu;
+
+public class Cpu {
+
+    double price;
+    // nested class
+    class Processor{
+
+        // members of nested class
+        double cores;
+        String manufacturer;
+
+        double getCache(){
+            return 4.3;
+        }
+    }
+    // nested protected class
+    protected class RAM{
+
+        // members of protected nested class
+        double memory;
+        String manufacturer;
+
+        double getClockSpeed(){
+            return 5.5;
+        }
+    }
+    public static void main(String[] args) {
+        
+      
+        // create object of Outer class CPU
+        Cpu cpu = new Cpu();
+
+       // create an object of inner class Processor using outer class
+        Cpu.Processor processor = cpu.new Processor();
+
+        // create an object of inner class RAM using outer class CPU
+        Cpu.RAM ram = cpu.new RAM();
+        System.out.println("Processor Cache = " + processor.getCache());
+        System.out.println("Ram Clock speed = " + ram.getClockSpeed());
+    
+    }
+    
+}
